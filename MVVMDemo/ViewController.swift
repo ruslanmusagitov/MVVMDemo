067e9120517172c996f59492119420bfd771ff3b
Ruslan Musagitov <ruslan.musagitov@gmail.com>
2019-05-15 10:20:45 +0600

    Initial Commit

---
+++ MVVMDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMDemo
+//
+//  Created by Ruslan Musagitov on 15/05/2019.
+//  Copyright © 2019 Ruslan Musagitov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
